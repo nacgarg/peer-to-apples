cb5aae5a96ef0be5ff2634f16a304d220909975b
AJ <aj@ajnadel.com>
2016-03-10 15:20:26 -0800

    the code style commit

--- game.rb
+++ game.rb
@@ -7,7 +7,6 @@ require 'socket'
 require 'openssl'
 require 'securerandom'
 
-
 require_relative 'deck.rb'
 
 class Game
@@ -15,19 +14,23 @@ class Game
 
 	def initialize
 		@peers = []
-		@initial_peer = request_input 'Initial Peer IP? ', false
 		@local_rsa = OpenSSL::PKey::RSA.new 2048
 		@local_id = Peer.hash_key @local_rsa.public_key
+
+    @initial_peer = request_input 'Initial Peer IP? '
 		@local_nickname = request_input 'Nickname? ', true
-		deckPath = request_input 'Path to deck? ',false
-		if !deckPath.nil?
+		deck_path = request_input 'Path to deck? '
+
+		if !deck_path.nil?
 			@deck = Deck.new
-			@deck.load_from_file deckPath
+			@deck.load_from_file deck_path
 		end
 	end
 
 	attr_reader :local_nickname
 	attr_reader :initial_peer
+  attr_reader :local_id
+  attr_reader :deck
 
 	def has_initial_peer
 		!@initial_peer.nil?
@@ -37,7 +40,7 @@ class Game
 		@local_rsa.public_key
 	end
 
-	def request_input(prompt, required)
+	def request_input(prompt, required=false)
 		print "#{prompt} "
 		loop do
 			s = STDIN.gets.strip
@@ -49,21 +52,18 @@ class Game
 	def has_deck
 		!@deck.nil?
 	end
-	def deck
-		@deck
-	end
+
 	def get_deck_hash
 		@deck.get_hash
 	end
+
 	def self.instance
 		@@instance ||= Game.new
 	end
+
 	def read_deck(data)
-		@deck=Deck.new
+		@deck = Deck.new
 		@deck.load_from_serialized(data)
-	end
-	def local_id
-		@local_id
 	end
 
 end
@@ -71,7 +71,7 @@ end
 class Peer < EventMachine::Connection
 	include EM::P::LineProtocol
 
-	GAME_PORT = ARGV[0].nil? ? 54484 : ARGV[0].to_i# class constant
+	GAME_PORT = 54484 if ARGV[0].nil? else ARGV[0].to_i  # class constant
 	@@peers = []
 
 	def self.hash_key(key)
@@ -86,6 +86,14 @@ class Peer < EventMachine::Connection
 		OpenSSL::PKey::RSA.new(key_s.gsub '|', "\n")
 	end
 
+  def initialize
+    @ready = false
+  end
+
+  attr_reader :ready
+  attr_reader :nickname
+  attr_reader :public_key
+
 	def identify_peer
 		pname = get_peername
 		if pname.nil?
@@ -95,6 +103,7 @@ class Peer < EventMachine::Connection
 			}
 			return false
 		end
+
 		port, ip = Socket.unpack_sockaddr_in(pname)
 		@peer_info = { # store because get_peername doesn't work in `#unbind`
 			:port => port,
@@ -103,24 +112,16 @@ class Peer < EventMachine::Connection
 		return true
 	end
 
-	def peer_is_ready_for_game_to_start
-		@ready.nil? ? false : @ready
-	end
-
 	def peer_info_s
 		"#{@peer_info[:ip]}:#{@peer_info[:port]}"
 	end
 
 	def ip_address
-		"#{@peer_info[:ip]}"
+		@peer_info[:ip]
 	end
 
 	def player_id
-		return Peer.hash_key(@public_key)
-	end
-
-	def public_key
-		@public_key
+		Peer.hash_key(@public_key)
 	end
 
 	def public_key_known
@@ -131,14 +132,9 @@ class Peer < EventMachine::Connection
 		Peer.hash_key(@public_key) if public_key_known
 	end
 
-
 	def identifying_name
 		return nil unless has_identified
 		"#{nickname}@#{hashed_key}"
-	end
-
-	def nickname
-		@nickname
 	end
 
 	def nickname_known
@@ -205,7 +201,7 @@ class Peer < EventMachine::Connection
 
 		@@peers << self
 		puts "Connected to peer #{peer_info_s}."
-		EM.add_timer(1) { send_action :gameserver_release, Game::SERVER_RELEASE }
+		EM.add_timer(1) { send_action :gameserver_release, Game::SERVER_RELEASE } # TODO sketch?
 	end
 
 	def unbind(possible_reason= "remote/unknown")
@@ -233,6 +229,7 @@ class Peer < EventMachine::Connection
 			reject_connection 'already has public key' 
 			return
 		end
+
 		begin
 			@public_key = key = Peer.socket_decode_key(data)
 		rescue OpenSSL::PKey::RSAError
@@ -247,14 +244,16 @@ class Peer < EventMachine::Connection
 
 	def send_deck_info
 		if !Game.instance.has_deck
-			puts "doesn't have a deck, so not sending hash"
+			puts "We don't have a deck, so not sending hash."
 			return
 		end
+
 		send_action :deck_hash, Game.instance.get_deck_hash
 	end
 
-	def read_deck_hash(data)
+	def read_deck_hash(peer_deck_hash)
 		if !Game.instance.has_deck
+      # TODO
 			#if has this deck hash on disk
 				#read from disk
 				#send action :has_deck, nil
@@ -263,7 +262,7 @@ class Peer < EventMachine::Connection
 			#end
 			return
 		end
-		if data==Game.instance.get_deck_hash
+		if peer_deck_hash == Game.instance.get_deck_hash
 			send_action :has_deck, nil 
 		else
 			reject_connection 'already got a different deck from someone else'
@@ -276,14 +275,13 @@ class Peer < EventMachine::Connection
 
 	def read_deck_contents(data)
 		Game.instance.read_deck(data)
-		#save this deck to disk for later
+		# TODO save this deck to disk for later
 		send_action :has_deck, nil
 	end
 
-	def peer_has_deck
-		puts "Peer has same deck, here we would send our peers"
-		#now we know that they have the same deck as us
-		#send them all our peers
+	def send_peers
+		# now we know that they have the same deck as us
+		# send them all our peers
 		
 		res = @@peers.select {|peer| peer != self }.map(&:ip_address).join ','
 		puts "Peers to send: #{res}"
@@ -291,21 +289,21 @@ class Peer < EventMachine::Connection
 	end
 
 	def received_peers(data)
-		puts "Received peers #{data}"
-		data.split(",").each do |ip|
-			puts "peer: #{ip}"
+		puts "Received peers: #{data}."
+		data.split(',').each do |ip|
+			puts "Considering peer: #{ip}"
 			if @@peers.any? { |peer| peer.ip_address == ip}
-				puts "already connected"
+				puts "Already connected to peer #{ip}."
 			else
-				puts "not connected yet"
+				puts "New peer #{ip} – connecting..."
 				connect_to_peer ip
 			end
-
 		end
 	end
 
-	def received_ready
-		@ready=true
+	def peer_ready
+		@ready = true
+    Peer.on_readiness_update
 	end
 
 	NICKNAME_CHARS_NOT_ALLOWED = /[^A-Za-z0-9_]/
@@ -362,100 +360,111 @@ class Peer < EventMachine::Connection
 		when :get_deck
 			send_deck
 		when :has_deck
-			peer_has_deck
+			send_peers
 		when :peers
 			received_peers incoming[:data]
 		when :ready
-			received_ready
-			Peer.on_readiness_update
+			peer_ready
 		end
-
-		
 	end
+
+  @@me_ready = false
 	def self.set_ready
-		@@peers.each { |peer| peer.send_action(:ready,nil)}
-		@@me_ready=true
+		@@peers.each { |peer| peer.send_action :ready, nil }
+		@@me_ready = true
 		Peer.on_readiness_update
 	end
-	@@me_ready = false
+
 	def self.on_readiness_update
-		if check_ready
-			puts "EVERYONE IS READY, LETS GO"
-			@@accepting_peers=false
-			puts "No longer accepting new peers because game is in progress"
-			game_start
-		else
-			puts "not all peers are ready, or I'm not ready =("
-		end
+		return if !check_ready
+
+		puts "EVERYONE IS READY. LET'S GO!"
+		@@accepting_peers = false
+		puts 'No longer accepting new peers, as the game is now in progress.'
+		game_start
 	end
+
 	def self.check_ready
 		return false unless @@me_ready
-		return false if @@peers.length==0
-		return false if @@peers.any? {|peer| !peer.peer_is_ready_for_game_to_start}
-		return true
+		return false unless has_peers
+		return false if @@peers.any? {|peer| !peer.ready}
+		true
 	end
+
 	def self.has_peers
-		@@peers.length!=0
+		@@peers.length > 0
 	end
+
 	def self.current_judge
-		@@judge_order[@@round_number % @@judgeOrder.size]
+		@@judge_order[@@round_number % @@judge_order.length]
 	end
+
 	def self.current_black_card
 		Game.instance.deck.black_cards[@@roundNumber % Game.instance.deck.black_cards.size]
 	end
+
 	def self.game_start 
-		@@round_number=0
-		hashed_keys=@@peers.map { |peer| peer.hashed_key }
+		@@round_number = 0
+
+		hashed_keys = @@peers.map { |peer| peer.hashed_key }
 		hashed_keys << Game.instance.local_id
 		hashed_keys.sort!
-		@@my_index=hashed_keys.index(Game.instance.local_id)
-		puts "My index: #{@@my_index}"
-		hashed_keys=hashed_keys.join ','
-		puts "Joined: #{hashed_keys}"
-		@@groupRandomSeed = Digest::SHA256.hexdigest(hashed_keys)
-		localRand=@@groupRandomSeed + ',' + Game.instance.local_id
-		@@localRandomSeed = Digest::SHA256.hexdigest(localRand)
-		puts "grouprandomseed: #{@@groupRandomSeed}"
-		puts "localRand: #{localRand}"
-		puts "localRandomSeed: #{@@localRandomSeed}"
-		Game.instance.deck.shuffle(@@groupRandomSeed)
-		@@judge_order=@@peers.map { |peer| peer.hashed_key }
+
+		@@my_index = hashed_keys.index(Game.instance.local_id)
+		puts "We are index #{@@my_index} of `hashed_keys`."
+
+		hashed_keys_joined = hashed_keys.join ','
+		puts "Hashed keys: #{hashed_keys_joined}"
+
+		@@group_random_seed = Digest::SHA256.hexdigest(hashed_keys_joined)
+    puts "Group random seed is #{@@group_random_seed}"
+
+		local_rand_base = @@group_random_seed + ',' + Game.instance.local_id
+		@@local_random_seed = Digest::SHA256.hexdigest(local_rand_base)
+		puts "Our local random base is #{local_rand_base}, converted to hex: #{@@localRandomSeed}"
+
+		Game.instance.deck.shuffle(@@group_random_seed)
+
+		@@judge_order = @@peers.map { |peer| peer.hashed_key }
 		@@judge_order << Game.instance.local_id
 		@@judge_order.sort!
-		@@judge_order.shuffle(random: prng_from_string(@@groupRandomSeed + "judgeOrder"))
-		puts "judgeOrder: #{@@judge_order}"
-		mySeg=Game.instance.deck.white_segment(@@peers.size+1,@@my_index)
-		puts "mySegment: #{mySeg}"
-		@@myRandom=Array.new
+		@@judge_order.shuffle(random: prng_from_string(@@group_random_seed + "judgeOrder"))
+		puts "The judge order (of hashed keys) is #{@@judge_order}"
+
+		my_segment = Game.instance.deck.white_segment(@@peers.size + 1, @@my_index)
+		puts "Our segment: #{my_segment}"
+
+		@@myRandom = Array.new
 		@@myHandIndexes=Array.new
-		numCards=8
-		if mySeg.size<numCards
-			numCards=mySeg.size
-		end
-		i=0
+
+		num_cards = my_segment.size if my_segment.size < num_cards else 8 # TODO Math.max ?
+
+		i = 0
+
 		loop do
-			rnd=SecureRandom.hex
-			cardIndex=@@localRandomSeed + ','+rnd + ','+i.to_s
+			rnd = SecureRandom.hex
+			cardIndex = @@localRandomSeed + ',' + rnd + ',' + i.to_s
 			puts cardIndex
-			cardIndex=int_from_str cardIndex
-			cardIndex=cardIndex%mySeg.size
+			cardIndex = int_from_str cardIndex
+			cardIndex = cardIndex % my_segment.size
 			if @@myHandIndexes.index(cardIndex).nil?
 				@@myHandIndexes<<cardIndex
 				@@myRandom<<rnd
 				i+=1
-				if(i==numCards)
+				if(i==num_cards)
 					break
 				end
 			end
 		end
+
 		puts "myRandom: #{@@myRandom}"
 		puts "myHandIndexes: #{@@myHandIndexes}"
-		@@myHand=@@myHandIndexes.map {|index| mySeg[index]}
+		@@myHand=@@myHandIndexes.map {|index| my_segment[index]}
 		puts "myHand: #{@@myHand}"
-		@@cardNonce=Array.new(numCards){ |i|
+		@@cardNonce=Array.new(num_cards){ |i|
 			SecureRandom.hex
 		}
-		@@hashedCard=Arary.new(numCards){ |i|
+		@@hashedCard=Arary.new(num_cards){ |i|
 			Digest::SHA256.hexdigest(@@myHandIndexes[i] + ',' + @@cardNonce[i])
 		}
 		puts "hashedCard: #{@@hashedCard}"
