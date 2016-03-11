41065d2f71d2f5ff9281bc43f8c0474cfd392663
Leif Jurvetson <leijurv@gmail.com>
2016-03-10 17:23:30 -0800

    weird error

--- game.rb
+++ game.rb
@@ -207,8 +207,10 @@ module ApplesToPeers
 			end
 			puts ""
 			puts "Here were the cards submitted, with the winner first"
-			@judge_decision.each {|card|
-				puts "Index #{card}: #{deck.white_cards[card]}"
+			@judge_decision.each {|cardIndex|
+				puts "I'm trying to get the #{cardIndex} th element of an array of length #{deck.white_cards.size}, but ruby won't let me. you see it won't print out the next line"
+				thisCard=deck.white_cards[cardIndex]
+				puts "Card: #{thisCard}"
 			}
 			puts ""
 			winner=@judge_decision[0]
